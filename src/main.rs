55d47695ebb1bac6025fe98e659b1405d4298509
Dan Jacob <danjac2018@gmail.com>
2024-01-21 14:59:52 +0200

    Read in directory and list file names

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{arg, command, value_parser};
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(
+            arg!(-d --dir <DIR>)
+                .required(true)
+                .value_parser(value_parser!(PathBuf)),
+        )
+        .get_matches();
+
+    if let Some(wallpaper_dir) = matches.get_one::<PathBuf>("dir") {
+        println!("Wallpaper {}", wallpaper_dir.display());
+        for file_name in wallpaper_dir
+            .read_dir()
+            .expect("Could not read this directory")
+        {
+            if let Ok(file_name) = file_name {
+                println!("{:?}", file_name.path());
+            }
+        }
+    }
 }
