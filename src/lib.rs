56f570d705909e690e50cdea895980fabcb2bfaf
Dan Jacob <danjac2018@gmail.com>
2024-01-22 10:14:10 +0200

    Tidy up

--- src/lib.rs
+++ src/lib.rs
@@ -36,41 +36,38 @@ fn matches_image_path(result: Result<DirEntry, Error>) -> Option<PathBuf> {
 }
 
 fn gsettings_set(schema: &str, key: &str, file_name: &str) -> Result<(), WallpaperError> {
-    match Command::new("gsettings")
+    Command::new("gsettings")
         .arg("set")
         .arg(schema)
         .arg(key)
         .arg(format!("file://{}", file_name))
         .output()
-    {
-        Err(err) => Err(WallpaperError::CommandError(err)),
-        _ => Ok(()),
-    }
+        .map_err(|e| WallpaperError::CommandError(e))?;
+    Ok(())
 }
 
 pub fn select_wallpaper(wallpaper_dir: &PathBuf) -> Result<PathBuf, WallpaperError> {
-    if let Ok(wallpaper_dir) = wallpaper_dir.read_dir() {
-        let paths: Vec<PathBuf> = wallpaper_dir.filter_map(matches_image_path).collect();
-        match paths.choose(&mut thread_rng()) {
-            Some(path) => Ok(path.clone()),
-            _ => Err(WallpaperError::ImageNotFound),
-        }
-    } else {
-        Err(WallpaperError::DirectoryNotFound)
+    let dir = wallpaper_dir
+        .read_dir()
+        .map_err(|_| WallpaperError::DirectoryNotFound)?;
+
+    let paths: Vec<PathBuf> = dir.filter_map(matches_image_path).collect();
+
+    match paths.choose(&mut thread_rng()) {
+        Some(path) => Ok(path.clone()),
+        _ => Err(WallpaperError::ImageNotFound),
     }
 }
 
 pub fn change_wallpaper(path: &PathBuf) -> Result<&PathBuf, WallpaperError> {
-    if let Some(file_name) = path.to_str() {
-        for (schema, key) in vec![
-            ("org.gnome.desktop.background", "picture-uri"),
-            ("org.gnome.desktop.background", "picture-uri-dark"),
-            ("org.gnome.desktop.screensaver", "picture-uri"),
-        ] {
-            gsettings_set(schema, key, file_name)?
-        }
-        Ok(path)
-    } else {
-        Err(WallpaperError::ImageNotFound)
+    let file_name = path.to_str().ok_or(WallpaperError::ImageNotFound)?;
+
+    for (schema, key) in vec![
+        ("org.gnome.desktop.background", "picture-uri"),
+        ("org.gnome.desktop.background", "picture-uri-dark"),
+        ("org.gnome.desktop.screensaver", "picture-uri"),
+    ] {
+        gsettings_set(schema, key, file_name)?
     }
+    Ok(path)
 }
