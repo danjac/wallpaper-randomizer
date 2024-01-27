d00bb07e67db94052ce89451aa6634182327a6e5
mo8it <mo8it@proton.me>
2024-01-27 15:46:39 +0100

    Embed variable in format string

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,7 @@ fn gsettings_set(schema: &str, key: &str, file_name: &str) -> Result<(), Wallpap
         .arg("set")
         .arg(schema)
         .arg(key)
-        .arg(format!("file://{}", file_name))
+        .arg(format!("file://{file_name}"))
         .output()
         .map_err(WallpaperError::CommandError)?;
     Ok(())
